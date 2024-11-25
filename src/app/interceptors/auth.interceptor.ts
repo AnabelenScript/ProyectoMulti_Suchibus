368cbba0c3301bbe34071a9aaebcaab131545bc7
Adrián10-07 <adrian2aguilarg@gmail.com>
2024-11-25 09:19:19 -0600

    choferes vistas

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -10,8 +10,6 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = localStorage.getItem('authToken');
-
-    // Añadir el token al encabezado si existe
     const clonedReq = token
       ? req.clone({ headers: req.headers.set('Authorization', `Bearer ${token}`) })
       : req;
@@ -19,7 +17,6 @@ export class AuthInterceptor implements HttpInterceptor {
     return next.handle(clonedReq).pipe(
       catchError((error: HttpErrorResponse) => {
         if (error.status === 401) {
-          // Redirigir al login si el servidor responde con un 401
           this.router.navigate(['/']);
         }
         return throwError(() => error);
